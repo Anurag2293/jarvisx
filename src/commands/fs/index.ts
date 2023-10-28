13e4a1bff28609a79c85382c60bfd0db905c5c8b
Anurag Dhote <anuragdhote392@gmail.com>
2023-10-28 10:40:39 +0530

    refactor - change name from ai to fs

--- src/commands/fs/index.ts
+++ src/commands/fs/index.ts
@@ -2,7 +2,7 @@ import {Args, Command, Flags} from '@oclif/core';
 import chalk from 'chalk';
 import { main } from '../../controllers/ai/index.js';
 
-export default class AI extends Command {
+export default class FS extends Command {
     static args = {
         prompt: Args.string({description: 'Ask AI to do any task', required: true}),
     }
@@ -12,7 +12,7 @@ export default class AI extends Command {
     }
 
     async run(): Promise<void> {
-        const {args, flags} = await this.parse(AI);
+        const {args, flags} = await this.parse(FS);
         const {success, message} = await main(args);
         if (success) {
             this.log(chalk.green.bold("SUCCESS: ") + message);
